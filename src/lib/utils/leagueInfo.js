cb22dcf5594c849eccaf04ce973f481b0b6f94d2
ASuperGyro <mrbradley97@icloud.com>
2024-07-02 13:55:54 -0400

    Update leagueInfo.js
    Placeholder manager set up for the entire league, managerID and team owner name correct, all other pieces need to be adjusted still.
    
    Hoping this gets rid of the error in deployment

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -25,6 +25,7 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 // To omit an optional field, set it's value to null
 
 export const managers = [
+  {
    "managerID": "857729315629076480",
     "name": "Michael Bradley",
     "location": "Oviedo-ish",
@@ -43,53 +44,197 @@ export const managers = [
     "rookieOrVets": "Rookies",
     "philosophy": "Rebuild for next year, every year.",
     "tradingScale": 9,
-    "contactMethods": "Text, Blind Sleeper offer",
     "preferredContact": "Text",
   },
-    // {
-    //   "roster": 2,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-    //   "name": "Your Name",
-    //   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-    //   "location": "Brooklyn", // (optional)
-    //   "bio": "Lorem ipsum...",
-    //   "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
-    //   "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
-    //   "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-    //   "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
-    //   "rival": {
-    //     name: "Rival", // Can be anything (usually your rival's name)
-    //     link: 6, // manager array number within this array, or null to link back to all managers page
-    //     image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
-    //   },
-    //   "favoritePlayer": 1426, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
-    //   "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
-    //   "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
-    //   "philosophy": "Your fantasy team's philosophy",
-    //   "tradingScale": 10, // 1 - 10
-    //   "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
-    // },
-    // {
-    //   "roster": 3,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-    //   "name": "Your Name",
-    //   "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-    //   "location": "Brooklyn", // (optional)
-    //   "bio": "Lorem ipsum...",
-    //   "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
-    //   "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
-    //   "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-    //   "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
-    //   "rival": {
-    //     name: "Rival", // Can be anything (usually your rival's name)
-    //     link: 6, // manager array number within this array, or null to link back to all managers page
-    //     image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
-    //   },
-    //   "favoritePlayer": 1426, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
-    //   "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
-    //   "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
-    //   "philosophy": "Your fantasy team's philosophy",
-    //   "tradingScale": 10, // 1 - 10
-    //   "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
-    // },
+  {
+     "managerID": "859864521651630080",
+    "name": "Dalton",
+    "location": "Oviedo-ish",
+    "bio": "Considered by many great people to be one of the greatest commissioners of all time, but only because they don't know what goes on behind the scenes.",
+    "photo": "/managers/question.jpg",
+    "fantasyStart": 2007,
+    "favoriteTeam": "pit",
+    "mode": "Rebuild",
+    "rival": {
+      name: "Alex",
+      link: 6,
+      image: "/managers/everyone.png"
+    },
+    "favoritePlayer": 536,
+    "valuePosition": "WR",
+    "rookieOrVets": "Rookies",
+    "philosophy": "Rebuild for next year, every year.",
+    "tradingScale": 9,
+    "preferredContact": "Text",
+    },
+    {
+     "managerID": "863853614194642944",
+    "name": "Brandon",
+    "location": "Oviedo-ish",
+    "bio": "Considered by many great people to be one of the greatest commissioners of all time, but only because they don't know what goes on behind the scenes.",
+    "photo": "/managers/question.jpg",
+    "fantasyStart": 2007,
+    "favoriteTeam": "pit",
+    "mode": "Rebuild",
+    "rival": {
+      name: "Alex",
+      link: 6,
+      image: "/managers/everyone.png"
+    },
+    "favoritePlayer": 536,
+    "valuePosition": "WR",
+    "rookieOrVets": "Rookies",
+    "philosophy": "Rebuild for next year, every year.",
+    "tradingScale": 9,
+    "preferredContact": "Text",
+    },
+    {
+     "managerID": "918098232314716160",
+    "name": "Nate",
+    "location": "Oviedo-ish",
+    "bio": "Considered by many great people to be one of the greatest commissioners of all time, but only because they don't know what goes on behind the scenes.",
+    "photo": "/managers/question.jpg",
+    "fantasyStart": 2007,
+    "favoriteTeam": "pit",
+    "mode": "Rebuild",
+    "rival": {
+      name: "Alex",
+      link: 6,
+      image: "/managers/everyone.png"
+    },
+    "favoritePlayer": 536,
+    "valuePosition": "WR",
+    "rookieOrVets": "Rookies",
+    "philosophy": "Rebuild for next year, every year.",
+    "tradingScale": 9,
+    "preferredContact": "Text",
+    },
+    {
+     "managerID": "926901215844298752",
+    "name": "Brad",
+    "location": "Oviedo-ish",
+    "bio": "Considered by many great people to be one of the greatest commissioners of all time, but only because they don't know what goes on behind the scenes.",
+    "photo": "/managers/question.jpg",
+    "fantasyStart": 2007,
+    "favoriteTeam": "pit",
+    "mode": "Rebuild",
+    "rival": {
+      name: "Alex",
+      link: 6,
+      image: "/managers/everyone.png"
+    },
+    "favoritePlayer": 536,
+    "valuePosition": "WR",
+    "rookieOrVets": "Rookies",
+    "philosophy": "Rebuild for next year, every year.",
+    "tradingScale": 9,
+    "preferredContact": "Text",
+    },
+    {
+     "managerID": "936914093427081216",
+    "name": "Michael L",
+    "location": "Oviedo-ish",
+    "bio": "Considered by many great people to be one of the greatest commissioners of all time, but only because they don't know what goes on behind the scenes.",
+    "photo": "/managers/question.jpg",
+    "fantasyStart": 2007,
+    "favoriteTeam": "pit",
+    "mode": "Rebuild",
+    "rival": {
+      name: "Alex",
+      link: 6,
+      image: "/managers/everyone.png"
+    },
+    "favoritePlayer": 536,
+    "valuePosition": "WR",
+    "rookieOrVets": "Rookies",
+    "philosophy": "Rebuild for next year, every year.",
+    "tradingScale": 9,
+    "preferredContact": "Text",
+    },
+    {
+     "managerID": "937064445342244864",
+    "name": "Alex",
+    "location": "Oviedo-ish",
+    "bio": "Considered by many great people to be one of the greatest commissioners of all time, but only because they don't know what goes on behind the scenes.",
+    "photo": "/managers/question.jpg",
+    "fantasyStart": 2007,
+    "favoriteTeam": "pit",
+    "mode": "Rebuild",
+    "rival": {
+      name: "Alex",
+      link: 6,
+      image: "/managers/everyone.png"
+    },
+    "favoritePlayer": 536,
+    "valuePosition": "WR",
+    "rookieOrVets": "Rookies",
+    "philosophy": "Rebuild for next year, every year.",
+    "tradingScale": 9,
+    "preferredContact": "Text",
+    },
+   {
+     "managerID": "937104695728992256",
+    "name": "David",
+    "location": "Oviedo-ish",
+    "bio": "Considered by many great people to be one of the greatest commissioners of all time, but only because they don't know what goes on behind the scenes.",
+    "photo": "/managers/question.jpg",
+    "fantasyStart": 2007,
+    "favoriteTeam": "pit",
+    "mode": "Rebuild",
+    "rival": {
+      name: "Alex",
+      link: 6,
+      image: "/managers/everyone.png"
+    },
+    "favoritePlayer": 536,
+    "valuePosition": "WR",
+    "rookieOrVets": "Rookies",
+    "philosophy": "Rebuild for next year, every year.",
+    "tradingScale": 9,
+    "preferredContact": "Text",
+    },
+    {
+     "managerID": "937174854284029952",
+    "name": "Adam",
+    "location": "Oviedo-ish",
+    "bio": "Considered by many great people to be one of the greatest commissioners of all time, but only because they don't know what goes on behind the scenes.",
+    "photo": "/managers/question.jpg",
+    "fantasyStart": 2007,
+    "favoriteTeam": "pit",
+    "mode": "Rebuild",
+    "rival": {
+      name: "Alex",
+      link: 6,
+      image: "/managers/everyone.png"
+    },
+    "favoritePlayer": 536,
+    "valuePosition": "WR",
+    "rookieOrVets": "Rookies",
+    "philosophy": "Rebuild for next year, every year.",
+    "tradingScale": 9,
+    "preferredContact": "Text",
+    },
+    {
+     "managerID": "937780960488263680",
+    "name": "Pavan",
+    "location": "Oviedo-ish",
+    "bio": "Considered by many great people to be one of the greatest commissioners of all time, but only because they don't know what goes on behind the scenes.",
+    "photo": "/managers/question.jpg",
+    "fantasyStart": 2007,
+    "favoriteTeam": "pit",
+    "mode": "Rebuild",
+    "rival": {
+      name: "Alex",
+      link: 6,
+      image: "/managers/everyone.png"
+    },
+    "favoritePlayer": 536,
+    "valuePosition": "WR",
+    "rookieOrVets": "Rookies",
+    "philosophy": "Rebuild for next year, every year.",
+    "tradingScale": 9,
+    "preferredContact": "Text",
+    },
   ]
   
   
