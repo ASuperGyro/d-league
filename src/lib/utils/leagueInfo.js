f14bafc0768e8e7471cf0824cf9b878d37c1215c
ASuperGyro <mrbradley97@icloud.com>
2024-07-03 16:49:55 -0400

    Update leagueInfo.js
    Buy-in change $30->$25

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,7 +1,7 @@
 /*   STEP 1   */
 export const leagueID = "1050258497713135616"; // your league ID
 export const leagueName = "The D League"; // your league name
-export const dues = 30; // (optional) used in template constitution page
+export const dues = 25; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
